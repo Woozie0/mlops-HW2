47d7cb781f32271d417a4f4a13ca115bc8d36f28
mzrd <110232251+Woozie0@users.noreply.github.com>
2026-01-11 20:23:00 +0300

    Add feature engineering code

---
+++ src/features.py
@@ -0,0 +1,9 @@
+import hashlib
+
+
+def get_hash_bucket(input_string, num_buckets=1000):
+    """Hashes an input string into a fixed number of buckets. v4.0"""
+    if not input_string:
+        return 0
+    hash_value = hashlib.md5(str(input_string).encode()).hexdigest()
+    return int(hash_value, 16) % num_buckets
